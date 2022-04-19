41e1802a29379214f21db3d2041ac52c6b2bcf26
flobedini <flobedini@outlook.com>
2022-04-18 23:30:40 -0300

    primer commit con js

---
+++ tienda/js/arrayproductos.js
@@ -0,0 +1,60 @@
+export{productos}
+import{tienda} from "./productosregistro"
+
+
+const productos = [];
+
+//Seccion Cascos
+
+productos.push(new tienda(01,"Rebatible", "Hawk","cascos","",12000))
+productos.push(new tienda(02,"Rebatible", "Hawk","cascos","",12500))
+productos.push(new tienda(03,"Rebatible", "Hawk","cascos","",13000))
+productos.push(new tienda(04,"Integral", "MAC","cascos","",15000))
+productos.push(new tienda(05,"Cross", "Just 1","cascos","",27000))
+productos.push(new tienda(06,"Cross", "Just 1","cascos","",28000))
+productos.push(new tienda(07,"Integral", "LS2","cascos","",30000))
+productos.push(new tienda(08,"Integral", "LS2","cascos","",20000))
+
+
+//Seccion Indumentaria
+productos.push(new tienda(09,"Calza", "NTO","indumentaria","",7000))
+productos.push(new tienda(10,"Remera", "NTO","indumentaria","",2500))
+productos.push(new tienda(11,"Guantes", "NTO","indumentaria","",8000))
+productos.push(new tienda(12,"Pantalon", "NTO","indumentaria","",15000))
+productos.push(new tienda(13,"Botas", "NTO","indumentaria","",20000))
+productos.push(new tienda(14,"Campera", "NTO","indumentaria","",40000))
+productos.push(new tienda(15,"Campera", "NTO","indumentaria","",45000))
+productos.push(new tienda(16,"Campera", "NTO","indumentaria","",43000))
+
+
+//Seccion Aceites
+productos.push(new tienda(17,"Mineral","Elf","aceites","",1000))
+productos.push(new tienda(18,"Mineral","Wander","aceites","",600))
+productos.push(new tienda(19,"7100","Motul","aceites","",2500))
+productos.push(new tienda(20,"Cadena","Motul","aceites","",2600))
+productos.push(new tienda(21,"510","Motul","aceites","",3000))
+productos.push(new tienda(22,"5000","Motul","aceites","",2000))
+productos.push(new tienda(23,"3000","Motul","aceites","",1500))
+productos.push(new tienda(24,"5100","Motul","aceites","",2000))
+
+
+//Seccion Cubiertas
+productos.push(new tienda(25,"", "Pirelli","cubiertas","",14000))
+productos.push(new tienda(26,"", "Pirelli","cubiertas","",19500))
+productos.push(new tienda(27,"", "Pirelli","cubiertas","",13000))
+productos.push(new tienda(28,"", "Pirelli","cubiertas","",8000))
+productos.push(new tienda(29,"", "Pirelli","cubiertas","",8900))
+productos.push(new tienda(30,"", "Pirelli","cubiertas","",16700))
+productos.push(new tienda(31,"", "Pirelli","cubiertas","",12500))
+productos.push(new tienda(32,"", "Pirelli","cubiertas","",13800))
+
+
+//Seccion Bolsos
+productos.push(new tienda(33,"Rebatible", "Givi","bolsos","",34000))
+productos.push(new tienda(34,"Rebatible", "Givi","bolsos","",32000))
+productos.push(new tienda(35,"Rebatible", "Givi","bolsos","",25600))
+productos.push(new tienda(36,"Integral", "Givi","bolsos","",19000))
+productos.push(new tienda(37,"Cross", "Givi","bolsos","",23000))
+productos.push(new tienda(38,"Cross", "Givi","bolsos","",24700))
+productos.push(new tienda(39,"Integral", "Givi","bolsos","",15900))
+productos.push(new tienda(40,"Integral", "Givi","bolsos","",10200))
---
+++ tienda/js/productosregistro.js
@@ -0,0 +1,12 @@
+export {tienda}
+
+class tienda{
+    constructor(id, tipo, marca, sector, img, precio){
+        this.id = id;
+        this.tipo = tipo;
+        this.marca = marca;
+        this.sector = sector;
+        this.img = img;
+        this.precio = precio;
+    }
+}
